d0634d3720952ec935f6b89fb947be10ef2add10
jarawin <newja2021@gmail.com>
2022-07-28 14:42:21 +0700

    ok

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { BsFillPersonFill, BsFillTelephoneFill } from "react-icons/bs";
-import { RiCarWashingFill } from "react-icons/ri";
+import React from 'react';
+import { BsFillPersonFill, BsFillTelephoneFill } from 'react-icons/bs';
+import { RiCarWashingFill } from 'react-icons/ri';
 
-import Calendar from "../../components/Calendar";
-import YingCenterBox from "./yingBooking/YingCenter";
-import YingLeftBox from "./yingBooking/YingLeftBox";
-import YingRightBox from "./yingBooking/YingRightBox";
+import Calendar from './Calendar';
+import YingCenterBox from './yingBooking/YingCenter';
+import YingLeftBox from './yingBooking/YingLeftBox';
+import YingRightBox from './yingBooking/YingRightBox';
 
 function Booking() {
   const data = [
-    "9:00 AM",
-    "10:00 AM",
-    "11:00 AM",
-    "12:00 PM",
-    "13:00 PM",
-    "14:00 PM",
-    "15:00 PM",
-    "16:00 PM",
-    "17:00 PM",
+    '9:00 AM',
+    '10:00 AM',
+    '11:00 AM',
+    '12:00 PM',
+    '13:00 PM',
+    '14:00 PM',
+    '15:00 PM',
+    '16:00 PM',
+    '17:00 PM',
   ];
 
   return (
