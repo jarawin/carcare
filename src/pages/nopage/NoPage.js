469d481b2e0b2758534f035933e0fa30b5096a2a
jarawin <newja2021@gmail.com>
2022-07-29 22:17:15 +0700

    ok

--- src/pages/nopage/NoPage.js
+++ src/pages/nopage/NoPage.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import Login from './Login';
+// import welcome from './welcome2.png';
 
 function NoPage() {
-  return <div></div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default NoPage;
