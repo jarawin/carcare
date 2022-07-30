dbb9d36fd0a3203a80113a833488698d16bcb0ba
yinyingyingip <yjunpen@gmail.com>
2022-07-30 15:56:14 +0700

    upupup

--- src/pages/booking/yingBooking/YingCenter2.js
+++ src/pages/booking/yingBooking/YingCenter2.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function YingCenter2() {
   const images = [
     {
-      title: 'IMG 1',
+      title: "IMG 1",
       idx: 0,
     },
     {
-      title: 'IMG 2',
+      title: "IMG 2",
       idx: 1,
     },
   ];
@@ -16,7 +16,7 @@ function YingCenter2() {
   const handleImage = (e) => {
     const name = e.target.name;
     console.log(name);
-    if (name == 'next') {
+    if (name == "next") {
       if (imgDisplay.idx < images.length - 1) {
         setImgDisplay(images[imgDisplay.idx + 1]);
       }
@@ -31,13 +31,13 @@ function YingCenter2() {
     <>
       <div className="flex justify-center">
         <button name="prev" className=" bg-lime-400" onClick={handleImage}>
-          {'<<<'}
+          {"<<<"}
         </button>
         <div className="mx-2 w-60 h-60 bg-sky-400 transition-transform	duration-500">
           {imgDisplay.title}
         </div>
         <button name="next" className="bg-lime-400 " onClick={handleImage}>
-          {'>>>'}
+          {">>>"}
         </button>
       </div>
     </>
