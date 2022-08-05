05ab77e81666d17303bfb6fbf0eca11a6b224c8a
jarawin <newja2021@gmail.com>
2022-08-05 18:45:51 +0700

    langHomeAdd

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -3,28 +3,31 @@ import { initReactI18next } from 'react-i18next';
 
 import { MenuEn, MenuTh } from '../languages/menu';
 import { NavEn, NavTh } from '../languages/navbar';
+import { HomeEn, HomeTh } from '../languages/home';
 
 const resources = {
-    en: {
-        translation: {
-            menu: MenuEn,
-            nav: NavEn,
-        },
+  en: {
+    translation: {
+      menu: MenuEn,
+      nav: NavEn,
+      home: HomeEn,
     },
-    th: {
-        translation: {
-            menu: MenuTh,
-            nav: NavTh,
-        },
+  },
+  th: {
+    translation: {
+      menu: MenuTh,
+      nav: NavTh,
+      home: HomeTh,
     },
+  },
 };
 
 i18n.use(initReactI18next).init({
-    resources,
-    lng: localStorage.getItem('language') ?? 'en',
-    interpolation: {
-        escapeValue: false,
-    },
+  resources,
+  lng: localStorage.getItem('language') ?? 'en',
+  interpolation: {
+    escapeValue: false,
+  },
 });
 
 export default i18n;
