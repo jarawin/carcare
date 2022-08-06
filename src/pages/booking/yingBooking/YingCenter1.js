e76ba4bddc2d34e2f0344a82d65ac79a22397f9d
yinyingyingip <yjunpen@gmail.com>
2022-08-06 14:20:52 +0700

    newup

--- src/pages/booking/yingBooking/YingCenter1.js
+++ src/pages/booking/yingBooking/YingCenter1.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function YingCenter1() {
   return (
@@ -8,14 +8,14 @@ function YingCenter1() {
         <div name="CenterBox" className="">
           <div className="container px-4 mx-auto max-w-5xl">
             <div className="text-center py-14">
-              <h1 className=" font-itim  text-7xl font-bold font-title text-sec text-red-700">
+              <h1 className=" font-itim mt-40 text-7xl font-bold font-title text-sec text-red-600 dark:text-red-500">
                 Car Washing
                 <br />
-                <span className=" text-black text-6xl font-itim ">
+                <span className=" text-black dark:text-white text-6xl font-itim ">
                   Online Booking Service
                 </span>
               </h1>
-              <p className=" text-xl max-w-sm mx-auto py-6">
+              <p className="dark:text-gray-300 text-xl max-w-sm mx-auto py-6">
                 Welcome to our car services site.
                 <br />
                 Let's book the service you are interested!
