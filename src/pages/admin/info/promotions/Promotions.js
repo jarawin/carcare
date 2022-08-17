d22c069f09a26d2caa7ae21f1c37f8b90ff261b5
jarawin <newja2021@gmail.com>
2022-08-17 16:14:07 +0700

    OK

--- src/pages/admin/info/promotions/Promotions.js
+++ src/pages/admin/info/promotions/Promotions.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 function Promotions() {
-  const column = ['Name'];
-  return <div>Promotions</div>;
+  const column = ['Name', 'tel'];
+  const data = [
+    ['ja1', '0911700623'],
+    ['ja2', '0987652021'],
+  ];
+
+  return (
+    <div class="overflow-x-auto relative shadow-md ">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            {column.map((item, index) => (
+              <th key={index} scope="col" class="py-3 px-6">
+                {item}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((item, index) => (
+            <tr
+              key={index}
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              {item.map((item, index) => (
+                <td
+                  key={index}
+                  class="py-4 px-6 font-semibold text-gray-900 dark:text-white"
+                >
+                  {item}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Promotions;
