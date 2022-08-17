a5c96e516791e284e5310685a50c528132572caf
yinyingyingip <yjunpen@gmail.com>
2022-08-17 16:22:37 +0700

    update pull1

--- src/pages/admin/info/promotions/Promotions.js
+++ src/pages/admin/info/promotions/Promotions.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Promotions() {
-  const column = ['Name'];
+  const promotions = [
+    "image, code, time_start, time_stop, timestamp, limit_type, limit_amount, promotion_id, is_public, description",
+  ];
+  const promotion_info = [
+    "promotion_id, service_id, type_of_car, discount_type, discount_baht",
+  ];
   return <div>Promotions</div>;
 }
 
