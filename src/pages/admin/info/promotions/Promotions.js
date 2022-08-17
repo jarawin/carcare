a938ee25fce09152d50e06734b56230dbace87a4
yinyingyingip <yjunpen@gmail.com>
2022-08-17 17:04:30 +0700

    push naja

--- src/pages/admin/info/promotions/Promotions.js
+++ src/pages/admin/info/promotions/Promotions.js
@@ -2,11 +2,40 @@ import React from "react";
 
 function Promotions() {
   const promotions = [
-    "image, code, time_start, time_stop, timestamp, limit_type, limit_amount, promotion_id, is_public, description",
+    "promotion_id",
+    "service_id",
+    "image",
+    "type_of_car",
+    "code",
+    "time_start",
+    "time_stop",
+    "timestamp",
+    "limit_type",
+    "limit_amount",
+    "discount_type",
+    "discount_baht",
+    "is_public",
+    "description",
   ];
-  const promotion_info = [
-    "promotion_id, service_id, type_of_car, discount_type, discount_baht",
+  const promotion_data = [
+    [
+      "promotion_id_1",
+      "service_id_1",
+      "image1",
+      "truck",
+      "aabbcc",
+      "9.30",
+      "16.00",
+      "12.40",
+      "1",
+      "10",
+      "percent",
+      "50 baht",
+      "public",
+      ">=500- baht",
+    ],
   ];
+  const promotion_info = [];
 
   const data = [
     ["ja1", "0911700623"],
@@ -18,7 +47,7 @@ function Promotions() {
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            {column.map((item, index) => (
+            {promotions.map((item, index) => (
               <th key={index} scope="col" class="py-3 px-6">
                 {item}
               </th>
@@ -27,7 +56,7 @@ function Promotions() {
         </thead>
 
         <tbody>
-          {data.map((item, index) => (
+          {promotion_data.map((item, index) => (
             <tr
               key={index}
               class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
