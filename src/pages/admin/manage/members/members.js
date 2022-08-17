0107e8dbc0975fde87a23d2550b3091a03a3af8f
yinyingyingip <yjunpen@gmail.com>
2022-08-17 21:29:38 +0700

    up pull3

--- src/pages/admin/manage/members/members.js
+++ src/pages/admin/manage/members/members.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function Members() {
+  const member = [
+    "user_id",
+    "serviceMember_id",
+    "mixPackage_id",
+    "start_time",
+    "end_time",
+  ];
+  const promotion_data = [["aa01", "sm005", "t01", "12.35", "15.35"]];
   return <div>members</div>;
 }
 
